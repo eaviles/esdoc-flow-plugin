42a5bf99e22760e72cf829ace5604ff437e61a5d
Tom Gallacher <tgallacher@nvidia.com>
2017-03-24 00:21:34 +0000

    Add support for parsing project .babelrc config.
    Any plugins defined in the project's .babelrc file is parsed and added
    to the min plugins required to support flowtype parsing. All duplicate
    plugins will be removed before running babel transform.
    
    If no .babelrc file exists in the project then only the default min plugins
    are used.

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const babel = require('babel-core');
+const fs = require('fs');
+const path = require('path');
 
-const babelOpts = {
-  plugins: ['transform-flow-strip-types', 'syntax-jsx']
+/**
+ * Remove duplicates from array
+ * @param  {array} items Input array
+ * @return {array} Output array with unique values
+ */
+const arrayUnique = (items) =>
+    items.filter((item, pos, self) => self.indexOf(item) === pos);
+
+/**
+ * Construct Babel plugins config object.
+ * Honours user's global Babel plugins config. and adds
+ * required Flowtype plugin if not already available.
+ *
+ * @return {Object}
+ */
+const buildBabelrcConfig = () => {
+  const defaultConfig = ['transform-flow-strip-types', 'syntax-jsx'];
+  const babelrcPath = path.normalize(path.join(__dirname, '..', '..', '.babelrc'));
+
+  if (! fs.existsSync(babelrcPath)) {
+    return { plugins: defaultConfig };
+  }
+
+  const babelrc = JSON.parse(fs.readFileSync(babelrcPath));
+
+  if (! Array.isArray(babelrc.plugins)) {
+    return { plugins: defaultConfig };
+  }
+
+  const plugins = arrayUnique(babelrc.plugins.concat(defaultConfig));
+
+  return { plugins };
 };
 
 exports.onHandleCode = (event) => {
+  const babelOpts = buildBabelrcConfig();
+
   try {
     const result = babel.transform(event.data.code, babelOpts);
     event.data.code = result.code;
